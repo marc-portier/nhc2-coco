4661a4d0cc3981f2550bb5d176d2c1418c065fdb
Marc Portier <marc.portier@gmail.com>
2021-12-19 17:12:49 +0100

    introduce a new __main__ to this module
    NOTE: this should at least take over the role of the *.tests.** in the end

---
+++ nhc2_coco/__main__.py
@@ -0,0 +1,216 @@
+from dotenv import load_dotenv
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, Namespace
+from collections import namedtuple
+import os
+import sys
+import asyncio
+import logging
+import logging.config
+import yaml
+from nhc2_coco.coco_discover_profiles import CoCoDiscoverProfiles
+from nhc2_coco import CoCo
+
+
+_LOGGER = logging.getLogger(__name__)
+DEFAULT_HOST = 'nhc2.local'
+DEFAULT_PORT = 8883
+
+
+def clout(*msg, em=False):
+    """Output actually intended as command-line response output. All other statements should use _LOGGER
+    :param *msg: message to output
+    :type *msg: str or tuple thereof
+    :param em: add emphasis (underline) to the message
+    :type em: bool
+    """
+    msg = ' '.join(map(lambda m: str(m), msg))
+    if em: msg = '\033[4m' + msg + '\033[0m'  # add emphasis by underlining the text
+    print(msg)
+
+
+async def do_discover(creds, args: Namespace):
+    """Performs NHC2_COCO Discovery
+    :param creds: Namedtuple holding the host:port and user:pass credentials
+    :param args: the parsed cli arguments
+    """
+    if creds.host is None:
+        clout('Searching for NiKo Home Control Controllers and profiles on them...')
+    else:
+        clout('Searching for NiKo Home Control Profiles on host [%s]' % creds.host)
+
+    disc = CoCoDiscoverProfiles(creds.host)
+    results = await disc.get_all_profiles()
+
+    clout('Discovery completed. Listing discovered Profiles for %d controller(s).\n' % (len(results)))
+
+    for i, result in enumerate(results):
+        clout('Controller #%d' % i, em=True)
+        ip, mac, profiles, host = tuple(result[k] for k in range(4))
+        clout(f' IP:   {ip}\n Host: {host}\n MAC:  {mac}')
+        clout(' %d profile(s) found.' % (len(profiles)))
+        for j, profile in enumerate(profiles):
+            clout(' Profile #%d:' % j)
+            uuid, name, type = tuple( profile.get(k) for k in ('Uuid','Name','Type'))
+            clout(f'  uuid: {uuid}\n  Name: {name}\n  Type: {type}')
+
+
+async def do_connect(creds, args):
+    _LOGGER.info("TODO -- complete implementation of connection test")
+
+    assert creds.host is not None, "Connection test requires a host to connect to."
+    clout(f"Testing connection to host '{creds.host}'")
+
+    coco = CoCo(creds.host, creds.user, creds.pswd, creds.port)
+    coco.connect() # how do you know connection is complete / succesful?
+    coco.disconnect()
+
+
+async def do_list(creds, args):
+    _LOGGER.info("TODO -- implement listing all found elements")
+    # todo allow specify TYPE of elements to list
+
+
+async def do_watch(creds, args):
+    _LOGGER.info("TODO -- implement watch mode reporting on all events")
+    # todo allow specify TYPE of elements to list
+    # listen for input keys --> disconnect on any key
+
+
+async def do_shell(creds, args):
+    _LOGGER.info("TODO -- implement an interactive shell to capture commands and 'talk' to the controller")
+    # need to consider some command syntax ? construct a language and use some parser-generator ?
+    # might be useful in a separate project?
+
+
+def get_arg_parser():
+    """ Defines the arguments to this module's __main__ cli script
+    by using Python's [argparse](https://docs.python.org/3/library/argparse.html)
+    """
+    ap = ArgumentParser(
+        prog='nhc2_coco',
+        description='CLI for nhc2_coco',
+        formatter_class=ArgumentDefaultsHelpFormatter,
+    )
+    ap.add_argument(
+        '-l', '--logconf',
+        metavar="<path to logconf file in yml format>",
+        type=str,
+        action='store',
+        help='where to move the logging to',
+    )
+    ap.add_argument(
+        '-H', '--host',
+        metavar="HOST",
+        action="store",
+        help='Specify host (name or ip) Niko-Home-Controller',
+    )
+    ap.add_argument(
+        '-p', '--port',
+        metavar="PORT",
+        action="store",
+        help='Specify portnumber',
+    )
+    ap.add_argument(
+        '-U', '--user',
+        metavar="USERNAME",
+        action="store",
+        help='User to authenticate',
+    )
+    ap.add_argument(
+        '-P', '--pswd',
+        metavar="PASSWORD",
+        action="store",
+        help='password to authenticate',
+    )
+
+    saps = ap.add_subparsers(
+        title='actions to perform',
+        required=True,
+        metavar="action",
+    )
+
+    saps.add_parser(
+        'discover',
+        aliases=['d', 'disc'],
+        help='Discover all nhc2 systems on the network',
+    ).set_defaults(func=do_discover)
+
+    saps.add_parser(
+        'connect',
+        aliases=['c', 'con'],
+        help='Test the connection to the controller',
+    ).set_defaults(func=do_connect)
+
+    saps.add_parser(
+        'list',
+        aliases=['l', 'ls'],
+        help='List all elements found on the controller'
+    ).set_defaults(func=do_list)
+
+    saps.add_parser(
+        'watch',
+        aliases=['w', 'wat'],
+        help='Watch and report all events on the controller'
+    ).set_defaults(func=do_watch)
+
+    saps.add_parser(
+        'shell',
+        aliases=['s', 'sh'],
+        help='Open an interactive shell to communicate to the controller'
+    ).set_defaults(func=do_shell)
+
+    return ap
+
+
+def credentials(args: Namespace):
+    """Returns a simple structure holding the to be applied credentials merged from CLI args and .env
+    """
+    return namedtuple("Credentials", ["host", "user", "pswd", "port"])(
+        host=args.host if args.host else os.environ.get('NHC2_HOST', DEFAULT_HOST),
+        port=args.port if args.port else os.environ.get('NHC2_PORT', DEFAULT_PORT),
+        user=args.user if args.user else os.environ.get('NHC2_USER'),
+        pswd=args.pswd if args.pswd else os.environ.get('NHC2_PASS'),
+    )
+
+
+def enable_logging(args: Namespace):
+    """Configures logging based on logconf specified through -l argument or .env ${NHC2_LOGCONF}
+    """
+    logconf=args.logconf if args.logconf else os.environ.get('NHC2_LOGCONF'),
+    if logconf is None or logconf == '':
+        return
+    import yaml   # conditional dependency -- we only need this (for now) when logconf needs to be read
+    with open(logconf, 'r') as yml_logconf:
+        logging.config.dictConfig(yaml.load(yml_logconf, Loader=yaml.SafeLoader))
+    _LOGGER.info(f"Logging enabled according to config in {logconf}")
+
+
+def main():
+    """ CLI entry function
+    """
+    exitcode = 0
+    load_dotenv()              # allow passing credentials and logging settings through dot-env strategy
+    ap = get_arg_parser()
+    args = ap.parse_args()     # interprete cli args
+    enable_logging(args)       # merge args and .env to enable logging
+    creds = credentials(args)  # merge args and .env to get credentials
+    _LOGGER.info(f"credentials => {str(creds)}")
+
+    # setup async wait construct for main routines
+    loop = asyncio.get_event_loop()
+    try:
+        # trigger the actual called action-function (async) and wait for it
+        loop.run_until_complete(args.func(creds, args))
+    except Exception as e:
+        _LOGGER.exception(e)
+        clout("***Error***", str(e))
+        ap.print_help()
+        exitcode = 1
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+        sys.exit(exitcode)
+
+
+if __name__ == "__main__":
+    main()
